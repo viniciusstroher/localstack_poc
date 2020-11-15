182cb26300ed04ee0bc9bdac99991e2e7d9dcaca
veni <viniciusstroher@gmail.com>
2020-11-15 02:31:33 -0300

    ajuste

--- startup-scripts/00_s3.sh
+++ startup-scripts/00_s3.sh
@@ -1,4 +1,4 @@
 awslocal s3 rm s3://huller1
 awslocal s3 mb s3://huller1
 awslocal s3api put-bucket-acl --bucket huller1 --acl public-read-write
-awslocal s3 cp D:\WebstormProjects\ionic-huller-app\hullerApp\build s3://huller1 --recursive
+awslocal s3 cp /app-to-upload s3://huller1 --recursive
