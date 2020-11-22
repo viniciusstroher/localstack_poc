643aab4d6846569761de8e8098636f08b5db35d8
veni <viniciusstroher@gmail.com>
2020-11-22 20:38:53 -0300

    remoção de arquivos nao utilizados

--- startup-scripts/04_lambda_auth.sh
+++ startup-scripts/04_lambda_auth.sh
@@ -1,10 +1,10 @@
 cd /api-auth/ && npm i aws-apigw-authorizer --save && npm i --save-dev @babel/cli @babel/core @babel/preset-env && npm run build && zip -qr -D ../api-auth.zip * && cd ..
-
+# --handler handler.endpoint \ usando o do babel
 awslocal lambda create-function \
     --function-name api-auth \
     --runtime nodejs12.x \
     --role admin \
-    --handler handler.endpoint \
+    --handler handler_es6.endpoint \
     --memory-size 512 \
     --timeout 360 \
     --environment Variables={NODE_OPTIONS="--experimental-modules"} \
